b2fc39c16f033594469119ed92844eb2ce1a6be3
Satyam Thakur <sk165344@gmail.com>
2025-08-21 15:14:37 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import expressProxy from "express-http-proxy";
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT;
+app.use(
+    "/auth",
+    expressProxy(`${process.env.AIRLINE_AUTH_SERVICE_URL}`, {
+        proxyReqPathResolver: (req) => `/auth${req.url}`,
+    })
+);
+app.use(
+    "/flight",
+    expressProxy(`${process.env.AIRLINE_FLIGHT_SERVICE_URL}`, {
+        proxyReqPathResolver: (req) => `/flight${req.url}`,
+    })
+);
+app.use(
+    "/booking",
+    expressProxy(`${process.env.AIRLINE_BOOKING_SERVICE_URL}`, {
+        proxyReqPathResolver: (req) => `/booking${req.url}`,
+    })
+);
+
+app.listen(PORT, () =>
+    console.log(`Gateway service is running on PORT ${PORT}`)
+);
